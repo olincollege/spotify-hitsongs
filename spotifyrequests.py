c02941439b9c2cb9856728c9606eaf75aa9ab0c2
Sparsh Gupta <sparshg@gmail.com>
2023-03-27 22:08:02 -0400

    Modified Spotify key obtainment

--- spotifyrequests.py
+++ spotifyrequests.py
@@ -1,10 +1,11 @@
 import base64
-import spotifyrequests
+import requests
+from spotify_api_keys import get_client_id, get_client_secret
 
 # set up API endpoint and client credentials
 url = 'https://accounts.spotify.com/api/token'
-client_id = '<your_client_id>'
-client_secret = '<your_client_secret>'
+client_id = get_client_id()
+client_secret = get_client_secret()
 
 # encode client credentials as base64
 credentials = f'{client_id}:{client_secret}'
@@ -20,14 +21,14 @@ data = {
 }
 
 # send POST request to /token endpoint
-response = spotifyrequests.post(url, headers=headers, data=data)
+response = requests.post(url, headers=headers, data=data)
 
 # check status code to ensure successful request
 if response.status_code == 200:
     # extract access token from response
     access_token = response.json()['access_token']
-    # do something with access token
-    print(access_token)
+    # print access token
+    print("Access token: ", access_token)
 else:
     # handle error
     print('Error:', response.status_code)
