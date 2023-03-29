5f5e48312fd4932354c8f2e9a984ed75e19058dc
Sparsh Gupta <sparshg@gmail.com>
2023-03-29 19:57:01 -0400

    Added visualization & unit tests, modified py code

--- spotify_hit_songs_test.py
+++ spotify_hit_songs_test.py
@@ -1,30 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import unittest
 import pandas as pd
 import os
 
 # Import the functions to test
-from spotify_data import get_spotify_data
-from spotify_hit_songs_visualizations import create_hit_song_visualizations
+from spotifydata import get_spotify_data
+from visualization import create_visualizations
+
+# Import API keys functions
+from spotify_api_keys import get_client_id, get_client_secret
+
+CLIENT_ID = get_client_id()
+CLIENT_SECRET = get_client_secret()
 
 
 class TestGetData(unittest.TestCase):
     
     def test_get_spotify_data(self):
+        """
+        Unit test function for the get_spotify_data function.
+
+        This function tests that the get_spotify_data function returns a
+        Pandas DataFrame with the expected columns,
+        contains at least one row, and contains no null values.
+        """
         # Test that the function returns a Pandas DataFrame
-        df = get_spotify_data()
+        df = get_spotify_data(CLIENT_ID, CLIENT_SECRET)
         self.assertIsInstance(df, pd.DataFrame)
         
         # Test that the DataFrame contains the expected columns
         expected_columns = ["playlist_name", "track_name", "track_album",
                             "track_artists", "track_release_date",
-                            "track_length", "track_popularity", "track_explicit",
-                            "track_markets", "track_album_type"]
+                            "track_length", "track_popularity",
+                            "track_explicit", "track_markets"]
         self.assertListEqual(list(df.columns), expected_columns)
         
         # Test that the DataFrame contains at least one row
@@ -32,25 +42,27 @@ class TestGetData(unittest.TestCase):
         
         # Test that the DataFrame contains no null values
         self.assertFalse(df.isnull().values.any())
-        
-    def test_create_hit_song_visualizations(self):
+
+    def test_create_hit_song_visualization(self):
+        """
+            Test function for create_visualizations
+        """
         # Test that the function returns None
-        df = pd.read_csv("data.csv")
-        result = create_hit_song_visualizations(df)
+        data_file = "data.csv"
+        output_dir = "figures"
+        result = create_visualizations(data_file, output_dir)
         self.assertIsNone(result)
-        
+
         # Test that the visualization files are created
-        self.assertTrue(os.path.exists("top_artists.png"))
-        self.assertTrue(os.path.exists("top_genres.png"))
-        self.assertTrue(os.path.exists("acousticness.png"))
-        self.assertTrue(os.path.exists("danceability.png"))
-        self.assertTrue(os.path.exists("energy.png"))
-        self.assertTrue(os.path.exists("instrumentalness.png"))
-        self.assertTrue(os.path.exists("liveness.png"))
-        self.assertTrue(os.path.exists("loudness.png"))
-        self.assertTrue(os.path.exists("speechiness.png"))
-        self.assertTrue(os.path.exists("tempo.png"))
-        self.assertTrue(os.path.exists("valence.png"))
-
-
-# Note: The unit tests assume that the working directory is set to the directory containing the Jupyter notebook and the two Python files (get_data.py and create_visualizations.py).
+        self.assertTrue(
+            os.path.exists(output_dir + "/popularity_distribution.png"))
+        self.assertTrue(
+            os.path.exists(output_dir + "/length_vs_popularity.png"))
+        self.assertTrue(
+            os.path.exists(output_dir + "/explicit_content_vs_popularity.png"))
+        self.assertTrue(
+            os.path.exists(output_dir + "/top_artists_by_popularity.png"))
+        self.assertTrue(
+            os.path.exists(output_dir + "/release_date_distribution.png"))
+        self.assertTrue(
+            os.path.exists(output_dir + "/available_markets_distribution.png"))
---
+++ visualization.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def create_visualizations(data_file, output_dir):
+    """
+    Generates various visualizations based on hit song data and saves them to
+    the specified output directory.
+
+    Args:
+        data_file (str): The path to the CSV file containing hit song data.
+        output_dir (str): The path to the directory where the
+            visualizations will be saved.
+
+    Returns:
+        None
+
+    Example:
+        create_visualizations('hit_songs.csv', 'visualization_outputs/')
+"""
+    # Load data
+    data = pd.read_csv(data_file)
+
+    print("\n")
+    print("=" * 50)
+    print("Plotting and Storing visualizations")
+    print("=" * 50)
+
+    # Create histogram
+    plt.hist(data['track_popularity'], bins=20)
+    plt.xlabel('Popularity')
+    plt.ylabel('Number of songs')
+    plt.title('Popularity Distribution (frequency)')
+    plt.savefig(output_dir + '/popularity_distribution.png')
+    plt.clf()
+
+    # Create scatter plot
+    plt.scatter(data['track_length'], data['track_popularity'])
+    plt.xlabel('Track Length (ms)')
+    plt.ylabel('Popularity')
+    plt.title('Track Length vs. Popularity')
+    plt.savefig(output_dir + '/length_vs_popularity.png')
+    plt.clf()
+
+    # Group data by explicit content and calculate mean popularity
+    mean_popularity = data.groupby('track_explicit')['track_popularity'].mean()
+    # Create bar chart
+    plt.bar(mean_popularity.index.astype(str), mean_popularity)
+    plt.xlabel('Explicit Content')
+    plt.ylabel('Mean Popularity')
+    plt.title('Explicit Content vs. Popularity')
+    plt.savefig(output_dir + '/explicit_content_vs_popularity.png')
+    plt.clf()
+
+    # Group data by artist and calculate mean popularity
+    mean_popularity = data.groupby('track_artists')['track_popularity'].mean()
+    # Sort by popularity and select top 10
+    top_artists = mean_popularity.sort_values(ascending=False).head(10)
+    # Create horizontal bar chart
+    plt.barh(top_artists.index, top_artists)
+    plt.xlabel('Mean Popularity')
+    plt.title('Top 10 Artists by Popularity')
+    plt.savefig(output_dir + '/top_artists_by_popularity.png')
+    plt.clf()
+
+    # Convert release date to datetime and group by year/month
+    data['release_date'] = pd.to_datetime(data['track_release_date'])
+    data['year_month'] = data['release_date'].dt.to_period('M')
+    release_counts = data['year_month'].value_counts().sort_index()
+    # Create line chart
+    plt.plot(release_counts.index.astype(str), release_counts)
+    plt.xlabel('Year/Month')
+    plt.ylabel('Number of Hit Songs')
+    plt.title('Release Date Distribution')
+    plt.xticks(rotation=90)
+    plt.savefig(output_dir + '/release_date_distribution.png')
+    plt.clf()
+
+    # Create scatter plot
+    plt.scatter(data['track_markets'], data['track_popularity'])
+    plt.xlabel('Number of Available Markets')
+    plt.ylabel('Popularity')
+    plt.title('Number of Markets vs Popularity')
+    plt.savefig(output_dir + '/available_markets_distribution.png')
+    plt.clf()
